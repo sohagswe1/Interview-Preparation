7a8870aeeb295887d8a266d2900bd1883a1e52e7
sohagswe <sohagpage@gmail.com>
2025-08-12 18:46:04 +0600

    Step-1

---
+++ Step - 01/Lecture 01/4_if-else.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int a;
+cin >> a;
+if(a>=35&& a<50) cout<<"grade D"<<endl;
+else if(a>=50 && a<70) cout<<"grade C "<<endl;
+else if (a>=70 && a<90)cout<<"grade B"<<endl;
+else if(a>=90)cout<<"grade A"<<endl;
+else cout<<"Fail"<<endl;
+
+}
