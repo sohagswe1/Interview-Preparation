be11f07b8943b7e492ff79e4bb70c236ed4b06a2
sohagswe <sohagpage@gmail.com>
2025-08-13 16:56:19 +0600

    Step-1

--- Step - 01/Lecture 02/5_Full_Pyramid.cpp
+++ Step - 01/Lecture 02/5_Full_Pyramid.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int i=1;i<n;i++){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
             cout<<" ";
         }
